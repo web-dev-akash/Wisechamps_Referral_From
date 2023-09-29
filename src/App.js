61a3cc3433c97c6ecc9f67ae4e8d62d572a625c0
web-dev-akash <akumar415129.ak@gmail.com>
2023-09-29 17:21:13 +0530

    Formatting and styling remaining

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const formDiv = document.getElementById("crmWebToEntityForm");
+  const query = new URLSearchParams(window.location.search).get("phone");
+  const [phone, setPhone] = useState(query);
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const getReferralUser = async (phone) => {
+    try {
+      setLoading(true);
+      const url = `https://backend.wisechamps.app/referral?phone=${phone}`;
+      const data = await fetch(url);
+      const res = await data.json();
+      if (res.message) {
+        setLoading(false);
+        setError(true);
+        return;
+      }
+      setUser(res.name);
+      formDiv.style.display = "block";
+      setLoading(false);
+      return;
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(true);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    // if (phone) {
+    //   getReferralUser(phone);
+    // }
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (!phone || !query || error) {
+    return (
+      <div>
+        <p>This Referral Link is not valid, please try again</p>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{user.toUpperCase()} referred you for this</p>
     </div>
   );
 }
