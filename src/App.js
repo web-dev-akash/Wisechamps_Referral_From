adc476d0ca3e91d843d75102db273b927621c5a8
web-dev-akash <akumar415129.ak@gmail.com>
2023-09-30 15:31:01 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-
+import logo from "./logo.png";
+import { AllRoutes } from "./Routes";
 function App() {
-  const formDiv = document.getElementById("crmWebToEntityForm");
-  const query = new URLSearchParams(window.location.search).get("phone");
-  const [phone, setPhone] = useState(query);
-  const [user, setUser] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const getReferralUser = async (phone) => {
-    try {
-      setLoading(true);
-      const url = `https://backend.wisechamps.app/referral?phone=${phone}`;
-      const data = await fetch(url);
-      const res = await data.json();
-      if (res.message) {
-        setLoading(false);
-        setError(true);
-        return;
-      }
-      setUser(res.name);
-      formDiv.style.display = "block";
-      setLoading(false);
-      return;
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-      setError(true);
-      return;
-    }
-  };
-
-  useEffect(() => {
-    // if (phone) {
-    //   getReferralUser(phone);
-    // }
-  }, []);
-
-  if (loading) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
-  if (!phone || !query || error) {
-    return (
-      <div>
-        <p>This Referral Link is not valid, please try again</p>
-      </div>
-    );
-  }
-
   return (
     <div className="App">
-      <p>{user.toUpperCase()} referred you for this</p>
+      <AllRoutes />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import logo from "./logo.png";
+export const Home = () => {
+  const formDiv = document.getElementById("crmWebToEntityForm");
+  const query = new URLSearchParams(window.location.search).get("phone");
+  const [phone, setPhone] = useState(query);
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const getReferralUser = async (phone) => {
+    try {
+      setLoading(true);
+      const url = `https://backend.wisechamps.app/referral?phone=${phone}`;
+      const data = await fetch(url);
+      const res = await data.json();
+      if (res.message) {
+        setLoading(false);
+        setError(true);
+        return;
+      }
+      setUser(res.name);
+      formDiv.style.display = "flex";
+      setLoading(false);
+      return;
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(true);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    if (phone) {
+      getReferralUser(phone);
+    }
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <svg viewBox="25 25 50 50">
+          <circle r="20" cy="50" cx="50"></circle>
+        </svg>
+      </div>
+    );
+  }
+
+  if (!phone || !query || error) {
+    return (
+      <div>
+        <p>This Referral Link is not valid, please try again</p>
+      </div>
+    );
+  }
+  return (
+    <>
+      <header className="animate__animated animate__fadeInLeft">
+        <img src={logo} alt="Wisechamps" width={"120px"} />
+      </header>
+      <h3 className="animate__animated animate__fadeInRight">
+        Welcome to Wisechampions Club
+      </h3>
+      <p className="animate__animated animate__fadeInLeft">
+        <b>{user.toUpperCase()}</b>
+        {"  "}
+        is enjoying our Live quizzes every week, they believe in the power of
+        interactive learning and thought you would enjoy it too
+      </p>
+      <p className="animate__animated animate__fadeInRight">
+        Fill out the form to join our WhatsApp Group
+      </p>
+    </>
+  );
+};
